431d5542a3099f541737021864fcdbea17a8326e
Davin Glick <Dav.glick18@gmail.com>
2016-11-22 16:36:53 +1100

    init repo

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+
+	requireAuthentication : function(req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+
+		console.log(req.method + " " + req.origionalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/About', middleware.requireAuthentication, function (req, res){ 
+	res.send('About Us!');
+
+});
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(PORT, function () { 
+console.log('express server started on port: ' + PORT );
+
+});
